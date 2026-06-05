0342905a62ed3f78e8d6f9b7d0b37b8fbef668c9
Rajesh Sawant <79257254+rajeshsawant98@users.noreply.github.com>
2026-06-04 23:21:09 -0700

    Add: max-area-of-island - submission-7

---
+++ Data Structures & Algorithms/max-area-of-island/submission-7.py
@@ -0,0 +1,31 @@
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        rows, cols = len(grid), len(grid[0])
+        MaxA = 0
+        visit = set()
+
+        def bfs(r,c):
+            A = 1
+            q = collections.deque()
+            q.append((r,c))
+            visit.add((r,c))
+
+            while q:
+                row, col = q.popleft()
+                directions = [[-1,0], [1,0],[0,-1],[0,1]]
+
+                for dr,dc in directions:
+                    r, c = row + dr , col + dc
+                    if r in range(rows) and c in range(cols) and grid[r][c] == 1 and (r,c) not in visit:
+                        A += 1
+                        q.append((r,c))
+                        visit.add((r,c))
+                
+            return A
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 1 and (r,c) not in visit:
+                    MaxA = max(MaxA, bfs(r,c))
+        
+        return MaxA
