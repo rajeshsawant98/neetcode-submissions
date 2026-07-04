f6b635148cc3250071549e15a5881cd941bee692
Rajesh Sawant <79257254+rajeshsawant98@users.noreply.github.com>
2026-07-03 20:47:40 -0700

    Add: permutation-string - submission-2

---
+++ Data Structures & Algorithms/permutation-string/submission-2.py
@@ -0,0 +1,26 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        count = Counter(s1)
+
+        l=0 
+        r = len(s1)-1
+
+        while r < len(s2):
+            Cop = count.copy()
+
+            for i in range(l,r+1):
+                if s2[i] in Cop:
+                    Cop[s2[i]] -=1
+                    if Cop[s2[i]] == 0:
+                        del Cop[s2[i]]
+                else:
+                    break
+            
+            if not Cop:
+                return True
+            l+=1
+            r+=1
+        
+        return False
+
+
