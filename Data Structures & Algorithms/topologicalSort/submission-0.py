d6c4017e7e075e1ba4eee75ebbbb915c8009553b
Rajesh Sawant <79257254+rajeshsawant98@users.noreply.github.com>
2026-08-06 11:34:51 -0700

    Add: topologicalSort - submission-0

---
+++ Data Structures & Algorithms/topologicalSort/submission-0.py
@@ -0,0 +1,35 @@
+class Solution:
+    def topologicalSort(self, n: int, edges: List[List[int]]) -> List[int]:
+        
+        adj = defaultdict(list)
+
+        for u,v in edges:
+            adj[u].append(v)
+        
+
+        Output = []
+        visited = set()
+        visiting = set()
+
+        def dfs(vert):
+            if vert in visited:
+                return True
+            
+            if vert in visiting:
+                return False
+            
+            visiting.add(vert)
+            for nei in adj[vert]:
+                if not dfs(nei):
+                    return False
+            
+            visited.add(vert)
+            visiting.remove(vert)
+            Output.append(vert)
+            return True
+        
+        for vert in range(n):
+            if not dfs(vert):
+                return []
+
+        return Output[::-1]
